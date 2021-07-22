76825408b167bdef4110c56a8947ba2d117654c9
Simone Gorini <el_sime@el-sime.net>
2021-07-22 21:53:44 +0200

    loop ported to ts

---
+++ src/ts/main.ts
@@ -0,0 +1,2 @@
+const nj = new ndjinn();
+nj.Start(); 
--- src/ts/ndjinn.ts
+++ src/ts/ndjinn.ts
@@ -1,4 +1,35 @@
-class ndjinn
-{
+class ndjinn {
+    startTime: number = window.performance.now();
+    isRunning: boolean = false;
+    currentFrameId: number = 0;
+    lastFrameTime: number = 0;
+    Start():void {
+        this.startTime = window.performance.now();
+        this.lastFrameTime = 0;
+        this.Run(null);
+    }
+    Stop():void {
+        this.isRunning = false;
+    }
     
+    Run(frameId: Number | null): void {
+        if (this.isRunning) {
+            const frameTime = window.performance.now() - this.startTime;
+            const deltaTime = (frameTime - this.lastFrameTime) 
+        }
+        this.Render();
+        this.currentFrameId = window.requestAnimationFrame((frameId: number) => this.Run(frameId));
+    }
+
+    Update(deltaTime: number): void {
+        console.log(deltaTime);
+    }
+
+    Render():void {
+        return;
+    }
+
+    Destroy() :void {
+        window.cancelAnimationFrame(this.currentFrameId);
+    }
 }
